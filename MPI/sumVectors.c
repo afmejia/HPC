2f224c0fe0b8cb160492f87a6960923b849852d0
utp <utp@utp>
2017-03-03 15:48:26 -0500

    Just playing with malloc and free

--- MPI/sumVectors.c
+++ MPI/sumVectors.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-const int size = 100;
+const int size = 1000000000;
 
-void fillVectors(int *A, int *B);
+void fillVectors(int *A);
 void printVector(int *V);
-void sumVectors(int *A, int *B, int *C);
+//void sumVectors(int *A, int *B, int *C);
 
 int main(int argc, char const *argv[]) {
-  int A[size];
-  int B[size];
-  int C[size];
-  fillVectors(A, B);
-  sumVectors(A, B, C);
-  printVector(C);
+  int *A = (int *) malloc(size * sizeof(int));
+  int *B = (int *) malloc(size * sizeof(int));
+  int *C = (int *) malloc(size * sizeof(int));
+//  int *C;
+//  B = (int *) malloc(size);
+//  C = (int *) malloc(size);
+  fillVectors(A);
+  fillVectors(B)
+  //sumVectors(A, B, C);
+  //printVector(A);
+
+  // printVector(B);
+  //printVector(C);
+  free(A);
+  //free(B);
+  //  free(C);
   return 0;
 }
 
-void fillVectors(int *A, int *B) {
+
+void fillVectors(int *A) {
   for (int i = 0; i < size; i++)  {
     A[i] = i + 1;
-    B[i] = i + 1;
   }
 }
 
 void printVector(int *V) {
   for (int i = 0; i < size; i++)
     printf("%d ", V[i]);
+  printf("\n");
 }
 
+/*
 void sumVectors(int *A, int *B, int *C) {
     for (int i = 0; i < size; i++)
       C[i] = A[i] + B[i];
-}
+}*/
