edd42154729a0e69ae0ba98df1d9af39b04e9747
Felipe <afmejia23@utp.edu.co>
2017-06-29 16:28:24 -0500

    querying properties from the cluster

--- Cuda/properties.cu
+++ Cuda/properties.cu
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  int dev_count;
-  cudaGetDeviceCount(&dev_count);
-  printf("There are %d cuda Devices\n", dev_count);
-  return 0;
+        int dev_count;
+        cudaGetDeviceCount(&dev_count);
+        printf("There are %d cuda Devices\n", dev_count);
+        cudaDeviceProp dev_prop;
+        for (int i = 0; i < dev_count; i++)
+        {
+                cudaGetDeviceProperties(&dev_prop, i);
+                printf("Device %d: \n", i);
+                printf("Maximum number of threads per block: %d \n", dev_prop.maxThreadsPerBlock);
+                printf("Number of SMs in the device: %d \n", dev_prop.multiProcessorCount);
+                printf("Clockrate: %d \n", dev_prop.clockRate);
+                printf("Maximum threads in x = %d, y = %d and z = %d \n",
+                       dev_prop.maxThreadsDim[0], dev_prop.maxThreadsDim[1],
+                       dev_prop.maxThreadsDim[2]);
+                printf("Maximum size of grid in x = %d, y = %d, z = %d \n",
+                       dev_prop.maxGridSize[0], dev_prop.maxGridSize[1],
+                       dev_prop.maxGridSize[2]);
+                printf("\n");
+        }
+        return 0;
 }
+
